415eab955f3cc9e16809a4e3c1ca82c685dd8126
murilomonteiro <muriloumonteiro@gmail.com>
2012-09-17 02:24:16 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleLogin::Application.config.secret_token = '1f31aa1f8d401f49a38c4ca2a3b0996b1cd7613c74357e4428673a3cbb615e230593b7aafc7421953bc9625cb4c521848602896e431356312a23b2de9f430162'
